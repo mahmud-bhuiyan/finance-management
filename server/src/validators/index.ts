7f267f1989d42c832a14f5dd10e7800e4dc00aa1
mahmud-bhuiyan <bh.mahmud@gmail.com>
2026-08-24 03:29:05 +0600

    chore: remove unused assets and components

--- server/src/validators/index.ts
+++
@@ -1,7 +0,0 @@
-// Zod schemas live in dedicated files (e.g. authValidators.ts).
-export { loginSchema, registerSchema } from "./authValidators.js";
-export {
-  createCompanyAdminSchema,
-  createTenantSchema,
-  updateTenantSchema,
-} from "./tenantValidators.js";
